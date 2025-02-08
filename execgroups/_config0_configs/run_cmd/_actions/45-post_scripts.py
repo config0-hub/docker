f88f92f64e593ffbad27375651334a0ee0b3a34f
gary <gary>
2025-02-08 09:21:09 +0800

    rendered and updated 02-08-2025-09-20

---
+++ execgroups/_config0_configs/run_cmd/_actions/45-post_scripts.py
@@ -0,0 +1,7 @@
+def default():
+
+    task = {'method': 'shelloutconfig',
+            'metadata': {'env_vars': [],
+                         'shelloutconfigs': ['config0-publish:::config0_core::post_scripts']}}
+
+    return task
